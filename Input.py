547f2f084c32dc8fdffdabdd6a6ee0d28af58245
TerrazasJr316 <160078635+TerrazasJr316@users.noreply.github.com>
2024-03-09 08:23:28 -0600

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,5 @@
+nom = input ("Dame tu nombre: ")
+#lo que hace la funcion input es siempre almacena datos de tipo String
+#habra que convertir el dato a tipo de dato numerico
+
+print ("Tu nombre es: " + nom)
