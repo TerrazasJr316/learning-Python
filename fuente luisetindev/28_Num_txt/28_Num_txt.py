0099b54b653fbe8c020aa4aa4d3992bbcf711b33
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-02 05:48:11 +0000

    git add luisetindev/28_Num_txt/28_Num_txt.py git add luisetindev/29_IMC.py git add luisetindev/30_Val_tarjeta_credito.py

--- fuente luisetindev/28_Num_txt/28_Num_txt.py
+++ fuente luisetindev/28_Num_txt/28_Num_txt.py
@@ -1,3 +1,9 @@
+"""
+Haz una funcion que cuente el numero
+de palabras que tiene un archivo de
+texto
+"""
+
 def cuenta_palabras(file_name):
     with open(file_name, 'r') as file:
         data = file.read()
