f7adae4fa0ee40668de8a9e56ed7bc156fc1540d
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-12 23:34:46 -0600

    python code

---
+++ 00_ejercicios_logica/41_dias_vividos.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def dias_vivo(fecha_nacimiento):
+    hoy = datetime.now()
+    nacimiento = datetime.strptime(fecha_nacimiento, "%d-%m-%Y")
+    dias_vivo = (hoy-nacimiento).days
+    return dias_vivo
+
+tu_fecha = input("Indroduce tu fecha de nacimiento (DIA-MES-AÑO): ")
+dias = dias_vivo(tu_fecha)
+print(f"Llevas {dias} dias vivo")
