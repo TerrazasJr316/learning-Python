1ace1fbf77910e5d51be189aece3b5c6bb877642
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-21 22:35:20 -0600

    README

--- 00_ejercicios_logica/52_numero_menor_recibida.py
+++ 00_ejercicios_logica/52_numero_menor_recibida.py
@@ -2,3 +2,11 @@
 Función que devuelva el numero menor de una lista recibida por parametros
 """
 
+def devolver_numero(lista):
+    minimo = lista[0]
+    for numero in lista:
+        if numero < minimo:
+            minimo = numero
+    return minimo
+
+print(devolver_numero([3,5,1,100,99,1000]))
