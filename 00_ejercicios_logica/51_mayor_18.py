b725df6ef9e067776080d2a295b3bce7557d078a
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-21 22:26:16 -0600

    README

---
+++ 00_ejercicios_logica/51_mayor_18.py
@@ -0,0 +1,10 @@
+"""
+Haz un programa que pida la peda y solo te deje entrar si tiene al menos 18 años
+"""
+
+edad = in6(input("Intoduce tu edad: "))
+
+if edad >= 18:
+    print("Estas dentro!")
+else:
+    print("No tienes la mayoria de edad")
