bfb8401ce47be7dc41e906e93e963b91b2eed62e
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-22 22:44:24 -0600

    python code

--- 02_session/00_varibales/01_practica_dos.py
+++ 02_session/00_varibales/01_practica_dos.py
@@ -1,3 +1,5 @@
+# FUNCIÓN --- INPUT ---
+
 nom = input ("Dame tu nombre: ")
 #lo que hace la funcion input es siempre almacena datos de tipo String
 #habra que convertir el dato a tipo de dato numerico
