defb0fd36c875fbbf8b26de160755027d6ab2963
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-23 23:23:26 -0600

    python code

---
+++ 02_session/Basic/00_varibales/01_Exercises/00_ejercicio_cinco.py
@@ -0,0 +1,8 @@
+"""
+ 8.- Escribir un programa que lea un entero positivo 'n',
+ introducido por el usuario y despues demuestre en pantalla
+ la sima de todos los enteros desde 1 hasta la suma de los
+ primeros enteros positivos puede ser calculada de la siguiente
+ forma: "suma = n(n + 1)/2"
+"""
+
