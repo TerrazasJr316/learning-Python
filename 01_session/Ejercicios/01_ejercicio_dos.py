c5631e89258dc34a264f1cc382332144523fc696
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-24 23:09:12 -0600

    variables con python

--- 01_session/Ejercicios/01_ejercicio_dos.py
+++ 01_session/Ejercicios/01_ejercicio_dos.py
@@ -1,7 +1,5 @@
 # 2.- Escribir mi nombre por separado por lineas
 
-nom = "Josue"
-ap = "Terrazas"
-am = "Mendoza"
-
-print (nom + "-" + ap + "-" + am)
+print("Josue")
+print("Terrazas")
+print("Mendoza")
