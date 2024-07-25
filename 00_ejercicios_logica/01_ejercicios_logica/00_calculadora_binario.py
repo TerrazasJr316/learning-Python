fcce60fd1a74669eb591d2a67519de3944e3eeb3
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-24 21:46:32 -0600

    python code

--- 00_ejercicios_logica/01_ejercicios_logica/00_calculadora_binario.py
+++ 00_ejercicios_logica/01_ejercicios_logica/00_calculadora_binario.py
@@ -7,6 +7,7 @@ import tkinter as tk
 
 def decimal_binary():
     try:
+        
         decimal_num = int(input_field.get())
         binary_num = bin(decimal_num).replace("0b", "")
         output_label.config(text = f"Binary: {binary_num}")
