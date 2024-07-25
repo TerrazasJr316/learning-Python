e9937b33df7b8db461ca24c4e25fdaca3e0aafcf
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-24 22:06:24 -0600

    python code

--- 01_session/Basic/00_varibales/00_Exercises/05_ejercicio_seis.py
+++ 01_session/Basic/00_varibales/00_Exercises/05_ejercicio_seis.py
@@ -1,10 +1,7 @@
 """
- 9.- Escrribir un programa que pida al usario
- su peso en (kg) y estatura en (m), cualcule
- el índice de masa corporal y lo almacene en
- una variable y muestre por pantalla la frase:
- "Ti índice de masa corporal es -- imc --" donde
- -- mc -- es el indice de mas corporal calculado
+ 9.- Escrribir un programa que pida al usario su peso en (kg) y estatura en (m), cualcule
+ el índice de masa corporal y lo almacene en una variable y muestre por pantalla la frase:
+ "Ti índice de masa corporal es -- imc --" donde -- mc -- es el indice de mas corporal calculado
  redondeado con dos decimales.
 """
 
