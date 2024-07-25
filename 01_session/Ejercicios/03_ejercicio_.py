3ecfa73791090d3834556565ddf2791a30ad5d16
Josue Terrazas Mendoza <160078635+TerrazasJr316@users.noreply.github.com>
2024-07-24 23:20:55 -0600

    python code

--- 01_session/Ejercicios/03_ejercicio_.py
+++
@@ -1,6 +0,0 @@
-# 4.- Escribir  un programa que el nombre del nombre en la consola y despues de que el usuario lo introduzca el nombre
-#por pantalla la cadena "Hola Mundo <nombre>" donde <nombre> que el usuario haya introducido
-
-nom = input ("Dame tu nombre: ")
-
-print ("Hola " + nom + " Bienvenido")
