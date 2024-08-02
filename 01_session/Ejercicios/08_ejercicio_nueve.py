a01d44715f73ad4a05669569fb085b256e965c7c
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-02 16:02:45 -0600

    Ejercicio 9

--- 01_session/Ejercicios/08_ejercicio_nueve.py
+++ 01_session/Ejercicios/08_ejercicio_nueve.py
@@ -1,8 +1,8 @@
 """
- 9.- Escrribir un programa que pida al usario su peso en (kg) y estatura en (m), cualcule
- el índice de masa corporal y lo almacene en una variable y muestre por pantalla la frase:
- "Ti índice de masa corporal es -- imc --" donde -- mc -- es el indice de mas corporal calculado
- redondeado con dos decimales.
+9.- Escrribir un programa que pida al usario su peso en (kg) y estatura en (m), cualcule
+el índice de masa corporal y lo almacene en una variable y muestre por pantalla la frase:
+"Ti índice de masa corporal es -- imc --" donde -- mc -- es el indice de mas corporal calculado
+redondeado con dos decimales.
 """
 
 peso = float(input("Dame tu peso en Kg: "))
