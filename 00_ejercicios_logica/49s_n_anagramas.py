a3fca8f04956e87d428f46f59b03f10456e3c9b1
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-10 23:30:02 -0600

    python code

---
+++ 00_ejercicios_logica/49s_n_anagramas.py
@@ -0,0 +1,16 @@
+"""
+Haz una función que reciba dos cadenas de texto y devuelva
+si son anagramas entre si o no
+"""
+
+def es_anagrama(p1, p2):
+    if len(p1) != len(p2):
+        return False
+    
+    if sorted(p1) == sorted(p2):
+        return True
+    else:
+        return False
+    
+print(es_anagrama("boca", "cabo"))
+print(es_anagrama("saul", "luis"))
