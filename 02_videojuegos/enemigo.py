278638722b8c383b4b2676f7b8de6b307f30c363
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-16 00:03:12 -0600

    python game

---
+++ 02_videojuegos/enemigo.py
@@ -0,0 +1,15 @@
+import pygame
+
+class cubo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 1
+        self.color = "red"
+        self.rect = pygame.Rect(self,x, self.y, self.ancho, self.alto)
+        
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana, self.color, self.rect)
+        self.rect = pygame.Rect(self,x, self.y, self.ancho, self.alto)
