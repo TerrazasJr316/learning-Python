f87f7c499bce6ebaed711af92955c034dc6ce442
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-19 00:18:06 -0600

    python game

--- 02_videojuegos/00_pygame.py
+++ 02_videojuegos/00_pygame.py
@@ -1,5 +1,7 @@
 import pygame
-from 01_personaje import cubo
+from personaje import Cubo
+
+
 ANCHO = 800
 ALTO = 600
 VENTANA = pygame.display.set_mode([ANCHO, ALTO])
@@ -8,14 +10,29 @@ jugando = True
 
 cubo = Cubo(100, 100)
 
+def gestionar_teclas(teclas):
+    if teclas[pygame.K_w]:
+        cubo.y -= cubo.velocidad
+    if teclas[pygame.K_s]:
+        cubo.y += cubo.velocidad
+    if teclas[pygame.K_a]:
+        cubo.x -= cubo.velocidad
+    if teclas[pygame.K_d]:
+        cubo.x += cubo.velocidad
+
 while jugando:
     eventos = pygame.event.get()
+    
+    teclas = pygame.key.get_pressed()
+    
+    gestionar_teclas(teclas)
     
     for evento in eventos:
         if evento.type == pygame.QUIT:
             jugando = False
-        
-    cubo.dibujar
+            
+    VENTANA.fill("white")
+    cubo.dibujar(VENTANA)
     pygame.display.update()
     
 quit()
