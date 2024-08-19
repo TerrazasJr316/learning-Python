92291213a9dee096f9f7656f6497ddc1255dc32d
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-18 23:24:16 -0600

    python game

---
+++ 02_videojuegos/00_pygame.py
@@ -0,0 +1,18 @@
+import pygame
+
+ANCHO = 800
+ALTO = 600
+VENTANA = pygame.display.set_mode([ANCHO, ALTO])
+
+jugando = True
+
+while jugando:
+    eventos = pygame.event.get()
+    
+    for evento in eventos:
+        if evento.type == pygame.QUIT:
+            jugando = False
+        
+    pygame.display.update()
+    
+quit()
