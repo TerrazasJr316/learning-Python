b280b546f43aeae2ac1d26c52d524f4dc7af09ae
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-18 23:36:54 -0600

    python game

--- 02_videojuegos/00_pygame.py
+++ 02_videojuegos/00_pygame.py
@@ -1,10 +1,12 @@
 import pygame
-
+from 01_personaje import cubo
 ANCHO = 800
 ALTO = 600
 VENTANA = pygame.display.set_mode([ANCHO, ALTO])
 
 jugando = True
+
+cubo = Cubo(100, 100)
 
 while jugando:
     eventos = pygame.event.get()
@@ -13,6 +15,7 @@ while jugando:
         if evento.type == pygame.QUIT:
             jugando = False
         
+    cubo.dibujar
     pygame.display.update()
     
 quit()
