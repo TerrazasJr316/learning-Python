ffedf85df8947149db6f31f9bbffe673d7dc2ed7
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-20 22:30:42 -0600

    Ejercicio 3

--- 01_session/Ejercicios/02_ejercicio_tres.py
+++
@@ -1,4 +0,0 @@
-# 3.- Poner en una variable tu nombre he imprimir el resultado 
-
-nom_completo = "Josue Terrazas Mendoza"
-print (nom_completo)
---
+++ 01_session/Ejercicios/Basic/02_ejercicio_tres.py
@@ -0,0 +1,4 @@
+# 3.- Poner en una variable tu nombre he imprimir el resultado 
+
+nom_completo = "Josue Terrazas Mendoza"
+print (nom_completo)
