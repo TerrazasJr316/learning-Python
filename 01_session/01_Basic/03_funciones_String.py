743b362a78e6e6f848fd266d3abe00ed0e5c83f7
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-22 22:59:50 -0600

    funciones String

---
+++ 01_session/01_Basic/03_funciones_String.py
@@ -0,0 +1,25 @@
+# Capitalize
+
+"""
+lo que hace esta función es convertir
+a mayúscula la primera leta de la
+variable que guarda una cadena de texto
+"""
+
+dir = "demo de la dirección uno"
+dir = dir.capitalize()
+
+print(dir)
+
+# Casefold
+
+"""
+Esta función lo que hace es convertir
+la variable de cadena de texto a minúscula
+si esque se encuentra algun caracter en mayúscula
+"""
+
+nom = "Josue Terrazas Mendoza"
+nom = nom.casefold()
+
+print(nom)
