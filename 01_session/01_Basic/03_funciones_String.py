e1d0858f0f55329fce792a85adc782289d699f6b
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-23 23:03:05 -0600

    funciones String

--- 01_session/01_Basic/03_funciones_String.py
+++ 01_session/01_Basic/03_funciones_String.py
@@ -23,3 +23,16 @@ nom = "Josue Terrazas Mendoza"
 nom = nom.casefold()
 
 print(nom)
+
+# Center
+
+"""
+Esta función se encarga de
+añadir un espacio de n ppixeles
+entre la primera letra y ultima
+de la cadena de texto
+"""
+dato = "Chevrolet"
+dato = dato.center(30)
+
+print(dato)
