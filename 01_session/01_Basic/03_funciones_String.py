83f1ee75ad4e9b19e74f1c47fdd2f0dd7e323ee4
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-08-25 23:12:59 -0600

    funciones String

--- 01_session/01_Basic/03_funciones_String.py
+++ 01_session/01_Basic/03_funciones_String.py
@@ -42,7 +42,11 @@ print(dato)
 """
 Permite contar el numero de palabras
 que se solicite dentro de una cedana
-de texto
+de texto.
+
+De igual manera de se puede agregar un
+intervalo de la cadena para busque la
+palabra en ese solo intervalo 
 """
 
 mensaje = "Las manzanas son rojas, las manzanas son mi fruta favorita y las manzanas verdes es la que mas me gusta"
