440f2bd6b4d7dac2b6cbbba79a385a646175f832
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-01 00:07:03 -0600

    funciones String

--- 01_session/01_Basic/03_funciones_String.py
+++ 01_session/01_Basic/03_funciones_String.py
@@ -87,3 +87,17 @@ example = "Esto es un ejemplo de texto"
 final = example.find("Esto")
 
 print(final)
+
+# format
+
+"""
+se puden usar n variables y con ellas
+darle el valor cualquiera que sea
+"""
+
+txt = "Mi nombre es {nom} y mi edad es de {edad}".format(nom = "Josué", edad = "21")
+print(txt)
+
+txt1 = "Mi nombre es {0} y mi edad es de {1}".format("Josué", 21)
+
+print(txt1)
