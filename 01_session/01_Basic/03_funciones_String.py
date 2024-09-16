60b2c42f9d2bf82679b537a1ccba2bdfad3bf3d1
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-16 00:11:54 -0600

    funciones String

--- 01_session/01_Basic/03_funciones_String.py
+++ 01_session/01_Basic/03_funciones_String.py
@@ -197,3 +197,17 @@ example_lstrip = "Python"
 result_lstrip = example_lstrip.lstrip()
 
 print("Bienvenido a " + example_lstrip + " lenguaje de alto nivel")
+
+# maketrans and translate
+
+example_maketrans = "Josue Terrazas1"
+result_maketrans = example_maketrans.maketrans("1", " ")
+
+print(example_maketrans.translate(result_maketrans))
+
+example_two_maketrans = "Josue Terrazas1234"
+example_num = "1234"
+example_num_change = "    "
+result_two_maketrans = example_maketrans.maketrans(example_num, example_num_change)
+
+print(example_maketrans.translate(result_maketrans))
