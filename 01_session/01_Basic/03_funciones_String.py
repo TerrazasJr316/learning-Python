cc9bc46d4de0fac1ec326cc092000e079b1b7de7
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-09-17 23:16:07 -0600

    funciones String

--- 01_session/01_Basic/03_funciones_String.py
+++ 01_session/01_Basic/03_funciones_String.py
@@ -214,4 +214,7 @@ print(example_maketrans.translate(result_maketrans))
 
 # partition
 
+example_partition = "Bienvenido, este mensaje esta hecho en Python"
+result_partition = example_partition.partition("este")
 
+print(result_partition)
