26a81482bec5a1c24510335746f7850907d9352e
Josue Terrazas <160078635+TerrazasJr316@users.noreply.github.com>
2024-10-21 20:43:33 -0600

    Delete directory

--- 00_ejercicios_logica/00_ejercicios/01 _Recursividad.py
+++
@@ -1,6 +0,0 @@
-def sigueme (n):
-    if n<100:
-        print(n)
-        sigueme(n+1)
-
-sigueme(1)
--- 00_ejercicios_logica/00_ejercicios/16_Bucle.py
+++
@@ -1,8 +0,0 @@
-for i in range(10):
-    print("Hola")
-
-i = 0
-
-while i < 10:
-    print("Hola")
-    i += 1
