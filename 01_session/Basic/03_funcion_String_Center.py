fec3c174ecb11a17a01b4df4825d0ddf07a2a95b
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-28 13:55:59 -0600

    Función Center

---
+++ 01_session/Basic/03_funcion_String_Center.py
@@ -0,0 +1,12 @@
+# center
+
+"""
+Esta función se encarga de
+añadir un espacio de n ppixeles
+entre la primera letra y ultima
+de la cadena de texto
+"""
+dato = "Chevrolet"
+dato = dato.center(30)
+
+print(dato)
