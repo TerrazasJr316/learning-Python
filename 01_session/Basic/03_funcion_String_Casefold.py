a166bbae5a75dcc12717a089c5facfb45af84a0b
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-28 13:55:05 -0600

    Función Casefold

---
+++ 01_session/Basic/03_funcion_String_Casefold.py
@@ -0,0 +1,12 @@
+# casefold
+
+"""
+Esta función lo que hace es convertir
+la variable de cadena de texto a minúscula
+si esque se encuentra algun caracter en mayúscula
+"""
+
+nom = "Josue Terrazas Mendoza"
+nom = nom.casefold()
+
+print(nom)
