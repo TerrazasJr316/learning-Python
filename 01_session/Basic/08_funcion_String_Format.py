caa651be7ac75c68ffb80ac84ae5281a023fb7a4
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-28 14:02:44 -0600

    Función Format

---
+++ 01_session/Basic/08_funcion_String_Format.py
@@ -0,0 +1,13 @@
+# format
+
+"""
+se puden usar n variables y con ellas
+darle el valor cualquiera que sea
+"""
+
+txt = "Mi nombre es {nom} y mi edad es de {edad}".format(nom = "Josué", edad = "21")
+print(txt)
+
+txt1 = "Mi nombre es {0} y mi edad es de {1}".format("Josué", 21)
+
+print(txt1)
