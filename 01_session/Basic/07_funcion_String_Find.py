d293139a1a1ff053b9fa78cd2929cf74b7565873
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-11-28 14:01:38 -0600

    Función Find

---
+++ 01_session/Basic/07_funcion_String_Find.py
@@ -0,0 +1,12 @@
+# find
+
+"""
+Se encarga de buscar dicha cadena
+de la variable que gurda la cadena de
+texto recorriendo caracter por carcter
+"""
+
+example = "Esto es un ejemplo de texto"
+final = example.find("Esto")
+
+print(final)
